02ef7b45bc6672e52da9f186376158069e5dee05
Hyberge <HertGrace@live.com>
2017-03-21 21:30:29 +0800

    增加行情快照的订阅功能

---
+++ Level2Quotes/Task/QuotationDataSubscriptionTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level2Quotes.Task
+{
+    enum SubscribeIntervalMode
+    {
+        SIM_OneMinInterval,
+        SIM_FiveMinInterval,
+        SIM_TenMinInterval,
+        SIM_FiftenMinInterval,
+        SIM_HalfHourInterval,
+    }
+    class QuotationDataSubscriptionTask: ITask
+    {
+        List<int> mSymbols = null;
+
+        public QuotationDataSubscriptionTask(ITask Next): base(Next)
+        { }
+
+        public void AddSymbolsList(List<int> Symbols)
+        {
+            mSymbols = Symbols;
+        }
+
+        public override bool TransactionProcessing()
+        {
+            bool ret = false;
+
+
+
+            return ret;
+        }
+    }
+}
