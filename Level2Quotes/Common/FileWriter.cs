141c460f1043307f45ebdb580c05b1051726f06b
Hyberge <HertGrace@live.com>
2017-03-22 22:39:05 +0800

    增加QuotationData的磁盘读写

--- Level2Quotes/Common/FileWriter.cs
+++ Level2Quotes/Common/FileWriter.cs
@@ -5,16 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.IO;
+using System.Windows;
 
 namespace Level2Quotes
 {
     class FileWriter
     {
         public static String sDataRootPath = "../../HTD/";
+        public static String sTransactionDataPath = sDataRootPath + "TransactionData/";
+        public static String sQuotationDataPath = sDataRootPath + "QuotationData/";
 
         public static bool IsTransactionDataExists(String Symbol, DateTime Day)
         {
-            String FileName = sDataRootPath + Symbol + "/" + Day.ToString("yyyy-MM-dd");
+            String FileName = sTransactionDataPath + Symbol + "/" + Day.ToString("yyyy-MM-dd");
 
             return File.Exists(FileName);
         }
@@ -23,7 +26,7 @@ namespace Level2Quotes
         {
             bool bRet = false;
 
-            String FilePath = sDataRootPath + Symbol;
+            String FilePath = sTransactionDataPath + Symbol;
             if (Directory.Exists(FilePath) == false)//如果不存在就创建file文件夹
             {
                 Directory.CreateDirectory(FilePath);
@@ -80,9 +83,10 @@ namespace Level2Quotes
         {
             Data.Clear();
 
-            String FileName = sDataRootPath + Symbol + "/" + Day.ToString("yyyy-MM-dd");
+            String FileName = sTransactionDataPath + Symbol + "/" + Day.ToString("yyyy-MM-dd");
             if (File.Exists(FileName) == false)
             {
+                MessageBox.Show("路径错误!!!");
                 return;
             }
 
@@ -110,6 +114,109 @@ namespace Level2Quotes
                         ele.IOType = Convert.ToInt32(strElement[6]);
                         ele.Channel = Convert.ToInt32(strElement[7]);
                         ele.TradingTime = Util.TradingTimeStringToInt(strElement[8]);
+
+                        Data.Add(ele);
+                    }
+                }
+            }
+
+            file.Close();
+        }
+
+        public static bool WriteQuotationDataToFile(String Symbol, QuotationData Data, bool Rewriting)
+        {
+            bool bRet = false;
+
+            String FilePath = sQuotationDataPath + Symbol;
+            if (Directory.Exists(FilePath) == false)//如果不存在就创建file文件夹
+            {
+                Directory.CreateDirectory(FilePath);
+            }
+
+            FileStream file = null;
+            String FileName = FilePath + "/" + DateTime.Now.ToString("yyyy-MM-dd");
+            if (File.Exists(FileName) == false)
+            {
+                file = File.Create(FileName);
+            }
+            else
+            {
+                file = new FileStream(FileName, FileMode.Open);
+            }
+
+            if (file.CanWrite)
+            {
+                if (Rewriting)
+                {
+                    file.SetLength(0);
+                }
+                else
+                {
+                    file.Seek(0, SeekOrigin.End);
+                }
+                StreamWriter writer = new StreamWriter(file);
+
+                String str = Data.TradingTime + "|";
+                str += Data.LastClose + "|";
+                str += Data.OpenPrice + "|";
+                str += Data.CurBidAmount + "|";
+                str += Data.AverBidPrice + "|";
+                str += Data.CurAskAmount + "|";
+                str += Data.AverAskPrice + "|";
+                str += Data.CancelBidNum + "|";
+                str += Data.CancelBidAmount + "|";
+                str += Data.CancelAskNum + "|";
+                str += Data.CancelAskAmount + "|";
+                str += Data.TotalBidNum + "|";
+                str += Data.TotalAskNum;
+
+                writer.WriteLine(str);
+
+                writer.Flush();
+            }
+
+            file.Close();
+
+            return bRet;
+        }
+
+        public static void ReadQuotationDataFromFile(String Symbol, DateTime Day, List<QuotationData> Data)
+        {
+            String FileName = sQuotationDataPath + Symbol + "/" + Day.ToString("yyyy-MM-dd");
+            if (File.Exists(FileName) == false)
+            {
+                MessageBox.Show("路径错误!!!");
+                return;
+            }
+
+            FileStream file = new FileStream(FileName, FileMode.Open);
+
+            if (file.CanRead)
+            {
+                StreamReader reader = new StreamReader(file);
+                String str;
+
+                while ((str = reader.ReadLine()) != null)
+                {
+                    string[] separator = new string[] { "|" };
+                    var strElement = str.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (strElement.Count() == 13)
+                    {
+                        QuotationData ele = new QuotationData();
+                        ele.TradingTime = strElement[0];
+                        ele.LastClose = (float)Convert.ToDouble(strElement[1]);
+                        ele.OpenPrice = (float)Convert.ToDouble(strElement[2]);
+                        ele.CurBidAmount = (float)Convert.ToDouble(strElement[3]);
+                        ele.AverBidPrice = (float)Convert.ToDouble(strElement[4]);
+                        ele.CurAskAmount = (float)Convert.ToDouble(strElement[5]);
+                        ele.AverAskPrice = (float)Convert.ToDouble(strElement[6]);
+                        ele.CancelBidNum = Convert.ToInt32(strElement[7]);
+                        ele.CancelBidAmount = (float)Convert.ToDouble(strElement[8]);
+                        ele.CancelAskNum = Convert.ToInt32(strElement[9]);
+                        ele.CancelAskAmount = (float)Convert.ToDouble(strElement[10]);
+                        ele.TotalBidNum = Convert.ToInt32(strElement[11]);
+                        ele.TotalAskNum = Convert.ToInt32(strElement[12]);
 
                         Data.Add(ele);
                     }
